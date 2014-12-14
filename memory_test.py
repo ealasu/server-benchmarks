4dfd9a7cf7d35ffa75ccd7de90f117ad1119f7c4
eman <e@m.an>
2014-12-13 17:08:28 -0700

    use python2

--- memory_test.py
+++ memory_test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import os
 from time import time
@@ -12,12 +12,12 @@ def run_test():
   # write a 100MB file
   with open('blob', 'wb') as f:
     chunk = os.urandom(1024)
-    for _ in range(BLOB_SIZE * 1024):
+    for _ in xrange(BLOB_SIZE * 1024):
       f.write(chunk)
 
   blobs = []
   start_time = time()
-  for i in range(MAX_BLOBS):
+  for i in xrange(MAX_BLOBS):
     elapsed = time() - start_time
     rss = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
     print('blobs: {}, elapsed seconds: {}, maxrss: {}'.format(
